8fbfbd775da0121ca48dd2d2c33a74bf126eb8b4
Kayiminus <kayimsaid@hotmail.fr>
2017-04-14 14:12:08 +0200

    Legere optimisation

--- y_k methodBRAD/y_k_method2.jl
+++ y_k methodBRAD/y_k_method2.jl
@@ -10,15 +10,13 @@ function y_k_method2(x,h)
   y=zeros(n)
   # on va commencer par initialiser la matrice qui va contenir nos epsilon pour les combinaisons de -1 et 1 et on va itérer à partir de ça
   matrice=matrice_eps(k)
-  for i in 1:n
-    sum=0 # on reinitalise la somme pour chaque x[i]
+    sum=zeros(n) # on reinitalise la somme pour chaque x[i]
     for m in 1:2^k # on  parcourt toutes les combinaison de -1 et 1
       vecteur_eps=matrice[m,:] # on recupere une combinaison dans la matrice
       someps=somme_eps(h,k,vecteur_eps) # on utilise la fonction somme_eps pour faire la somme des element  du vecteur avec le h corespondant
-      sum=sum+(x[i]+someps)^(k-1)*sign(x[i]+someps)*produit(vecteur_eps) #on realise l'opération principale
+      sum=sum+((x+someps).^(k-1)).*sign(x+someps).*produit(vecteur_eps) #on realise l'opération principale
     end
-  y[i]=sum/(2^(k+1)*produit(h)*factorial(k-1)) # on divise
-  end
+  y=sum/(2^(k+1)*produit(h)*factorial(k-1)) # on divise
 
   return y # on renvoie le vecteur y
 end
