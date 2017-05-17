bc4855a7d3c21236135a6628b6ac452c4cbbdf6d
HaroldGal <harold.gallice@gmail.com>
2017-05-17 10:58:35 +0200

    test time imbriqué

--- script_gen_y_k.jl
+++ script_gen_y_k.jl
@@ -2,10 +2,11 @@
 include("y_k methodBRAD/y_k_method3.jl")
 include("Traces_experimentales/genA.jl")
 include("y_k methodBRAD/display_ovale.jl")
-k=12
+include("y_k methodBRAD/y_k_method_imbrique.jl")
+k=20
 #----- Creation des propriétés initiales du filtre
 n = 10 # taille de nos matrice propriétés du filtre
-A = get_matrix_vp_petite(n)
+A = get_matrix_vp_grande(n)
 srand(10) # pour poser une graine random pour les tests
 b = rand(n,1)
 c = rand(1,n)
@@ -20,11 +21,23 @@ for i in 2:k
   A_puissance *= A
 end
 #----------------------------------------------
-pas = 0.01
+s = 0
+for i in k+1:10000
+  s += abs((c*A_puissance*b)[1])
+  A_puissance *= A
+end
+#---------------------
+pas = 1
 x = collect(-sum(h):pas:sum(h))
+print("Temps execution methode non imbriqué : ")
 tic()
 y = y_k_method3(x,h)
 toc()
+print("Temps execution methode non imbriqué : ")
+tic()
+y = y_k_method_imbrique(x,h)
+toc()
 using PyPlot
+
 plot(x,y)
-display_ovale(h,A,b,c)
+#display_ovale(h,A,b,c,s)
