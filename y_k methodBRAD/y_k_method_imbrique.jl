28778a2a323416e1454aefff50915a8388a6a301
KayimSaidB <kayimsaid@hotmail.fr>
2017-05-17 11:40:00 +0200

    petit pb réparé

--- y_k methodBRAD/y_k_method_imbrique.jl
+++ y_k methodBRAD/y_k_method_imbrique.jl
@@ -23,26 +23,21 @@ function y_k_method_imbrique(x,h)
                 someps5=T[1][((z>>(4*(1-1)))&15)+1]
                 someps=someps1+someps2+someps3+someps4+someps5
                 sum=sum+((x+someps).^(k-1)).*sign(x+someps).*(((k-count_ones(m))%2==k_parite)?1:-1) #on realise l'opération principale
-            
 
-                end
                 m=m+1
+              end
 
                 someps=0
               end
             end
           end
         end
-
-
-    end
-println(m)
   y=sum/(2^(k+1)*produit(h)*factorial(k-1)) # on divise
 
   return y # on renvoie le vecteur y
 end
 
-
+#=
 h= ones(20)
 T = gen_T(h,4)
 x=collect(-3:0.1:3)
@@ -50,3 +45,4 @@ y = y_k_method3(x,h)
 print("Finished")
 using PyPlot
 plot(x,y)
+=#
